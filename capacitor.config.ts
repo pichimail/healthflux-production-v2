7bb7b559dd9dee0d5d3d5f5adf2f826c0fed1b5b
pichimail <pichimail24@gmail.com>
2026-05-10 14:57:27 +0530

    Add initial iOS project files and configurations
    - Created LaunchScreen.storyboard for app launch screen.
    - Created Main.storyboard for the main app interface.
    - Added Info.plist with essential app configurations.
    - Introduced .gitignore for CapApp-SPM to exclude unnecessary files.
    - Added Package.swift for Swift Package Manager dependencies.
    - Created README.md for CapApp-SPM with usage instructions.
    - Implemented CapApp-SPM.swift to define app-specific constants.
    - Added debug.xcconfig to manage debug configurations.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.synclo.app',
+  appName: 'healthflux',
+  webDir: 'dist'
+};
+
+export default config;
